ab4f23c546c097c323ead2613dfb05b81a32aa58
hashanDhananjaya <68744920+hashanDhananjaya@users.noreply.github.com>
2021-06-05 20:45:14 +0530

    Add files via upload

---
+++ Subject/SubjectMain.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace CollegeCore.Subject
+{
+    partial class SubjectMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSubjectChild = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.icnBtnAddLec = new FontAwesome.Sharp.IconButton();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSubjectChild
+            // 
+            this.panelSubjectChild.BackColor = System.Drawing.Color.White;
+            this.panelSubjectChild.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelSubjectChild.Location = new System.Drawing.Point(0, 42);
+            this.panelSubjectChild.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.panelSubjectChild.Name = "panelSubjectChild";
+            this.panelSubjectChild.Size = new System.Drawing.Size(1180, 641);
+            this.panelSubjectChild.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Controls.Add(this.icnBtnAddLec);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(100, 10, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1180, 42);
+            this.panel1.TabIndex = 5;
+            // 
+            // icnBtnAddLec
+            // 
+            this.icnBtnAddLec.BackColor = System.Drawing.Color.Gray;
+            this.icnBtnAddLec.Dock = System.Windows.Forms.DockStyle.Left;
+            this.icnBtnAddLec.FlatAppearance.BorderSize = 0;
+            this.icnBtnAddLec.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.icnBtnAddLec.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.icnBtnAddLec.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.icnBtnAddLec.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.icnBtnAddLec.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.icnBtnAddLec.IconColor = System.Drawing.Color.Black;
+            this.icnBtnAddLec.IconSize = 16;
+            this.icnBtnAddLec.Location = new System.Drawing.Point(0, 0);
+            this.icnBtnAddLec.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.icnBtnAddLec.Name = "icnBtnAddLec";
+            this.icnBtnAddLec.Rotation = 0D;
+            this.icnBtnAddLec.Size = new System.Drawing.Size(1151, 42);
+            this.icnBtnAddLec.TabIndex = 0;
+            this.icnBtnAddLec.Text = "Add Subjects";
+            this.icnBtnAddLec.UseVisualStyleBackColor = false;
+            this.icnBtnAddLec.Click += new System.EventHandler(this.add_Subject_click);
+            // 
+            // SubjectMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1180, 683);
+            this.Controls.Add(this.panelSubjectChild);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "SubjectMain";
+            this.Text = "SubjectMain";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSubjectChild;
+        private System.Windows.Forms.Panel panel1;
+        private FontAwesome.Sharp.IconButton icnBtnAddLec;
+    }
+}
---
+++ TimeTable/TimeTableMain.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CollegeCore.Infrastructure;
+using CollegeCore.Utilities;
+using CollegeManagement.Server.Infrastructure;
+//using Google.Protobuf.WellKnownTypes;
+
+namespace CollegeManagement.TimeTable
+{
+    public partial class TimeTableMain : Form
+    {
+        TimeTableGeneratorToPDFCore timeTable = new TimeTableGeneratorToPDFCore();
+
+        public TimeTableMain()
+        {
+
+            InitializeComponent();
+            Cursor.Current = Cursors.WaitCursor;
+            LoadData();
+            Cursor.Current = Cursors.Default;
+        }
+
+        private void LoadData()
+        {
+            comboBoxStudent.DataSource = timeTable.GetData(CommonConstants.QUERY_GET_SUBGROUPS);
+            comboBoxLecturer.DataSource = timeTable.GetData(CommonConstants.QUERY_GET_LECTURERS);
+            comboBoxRoom.DataSource = timeTable.GetData(CommonConstants.QUERY_GET_ROOMS);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var fbd = new SaveFileDialog();
+            fbd.RestoreDirectory = true;
+            fbd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            fbd.Filter = "Pdf File|*.pdf";
+            fbd.FileName = "Time-Table-Generated-at-" + DateTime.Now.ToString("yyyyMMddHHmmssffff");
+
+            Exception status = null;
+
+            DialogResult result = fbd.ShowDialog();
+
+            if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.FileName))
+            {
+                Cursor.Current = Cursors.WaitCursor;
+
+                if (radioButtonLecturer.Checked == true) 
+                {
+                   status = timeTable.GenerateTimeTable(fbd.FileName, CommonConstants.QUERY_GET_TIMETABLE_BY_LECTURER, comboBoxLecturer.SelectedItem.ToString(), "LEC");
+                
+                }
+                else if (radioButtonRoom.Checked == true)
+                {
+                    status = timeTable.GenerateTimeTable(fbd.FileName, CommonConstants.QUERY_GET_TIMETABLE_BY_ROOM, comboBoxRoom.SelectedItem.ToString(), "HALL");
+                }
+                else if (radioButtonStudent.Checked == true)
+                {
+                    status = timeTable.GenerateTimeTable(fbd.FileName, CommonConstants.QUERY_GET_TIMETABLE_BY_STUDENT_GROUP, comboBoxStudent.SelectedItem.ToString(), "GROUP");
+                }
+                
+
+                Cursor.Current = Cursors.Default;
+
+                if (status != null)
+                {
+                    MessageBox.Show("Error Genarating PDF!!\n" + status.Message.ToString(), "Generate Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+
+            
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonStudent.Checked == true)
+            {
+                comboBoxStudent.Enabled = true;
+            }
+            else 
+            {
+                comboBoxStudent.Enabled = false;
+            }
+        }
+
+        private void radioButtonLecturer_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonLecturer.Checked == true)
+            {
+                comboBoxLecturer.Enabled = true;
+            }
+            else
+            {
+                comboBoxLecturer.Enabled = false;
+            }
+        }
+
+        private void radioButtonRoom_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonRoom.Checked == true)
+            {
+                comboBoxRoom.Enabled = true;
+            }
+            else
+            {
+                comboBoxRoom.Enabled = false;
+            }
+        }
+    }
+}
