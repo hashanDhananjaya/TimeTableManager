0ee97f831ceea8bc12b73b0ccac432fdd95e8dfb
hashanDhananjaya <68744920+hashanDhananjaya@users.noreply.github.com>
2021-06-05 20:46:06 +0530

    Add files via upload

---
+++ Student/StudentMain.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace CollegeCore.Student
+{
+    partial class StudentMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.icnBtnYearSem = new FontAwesome.Sharp.IconButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.iconButton1 = new FontAwesome.Sharp.IconButton();
+            this.icnBtnGroup = new FontAwesome.Sharp.IconButton();
+            this.icnBtnProgram = new FontAwesome.Sharp.IconButton();
+            this.panelStudentChild = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // icnBtnYearSem
+            // 
+            this.icnBtnYearSem.BackColor = System.Drawing.Color.Gray;
+            this.icnBtnYearSem.Dock = System.Windows.Forms.DockStyle.Left;
+            this.icnBtnYearSem.FlatAppearance.BorderSize = 0;
+            this.icnBtnYearSem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.icnBtnYearSem.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.icnBtnYearSem.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.icnBtnYearSem.ForeColor = System.Drawing.Color.White;
+            this.icnBtnYearSem.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.icnBtnYearSem.IconColor = System.Drawing.Color.Black;
+            this.icnBtnYearSem.IconSize = 16;
+            this.icnBtnYearSem.Location = new System.Drawing.Point(0, 0);
+            this.icnBtnYearSem.Name = "icnBtnYearSem";
+            this.icnBtnYearSem.Rotation = 0D;
+            this.icnBtnYearSem.Size = new System.Drawing.Size(325, 42);
+            this.icnBtnYearSem.TabIndex = 0;
+            this.icnBtnYearSem.Text = "Year / Semester Management";
+            this.icnBtnYearSem.UseVisualStyleBackColor = false;
+            this.icnBtnYearSem.Click += new System.EventHandler(this.icnBtnYearSem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.iconButton1);
+            this.panel1.Controls.Add(this.icnBtnGroup);
+            this.panel1.Controls.Add(this.icnBtnProgram);
+            this.panel1.Controls.Add(this.icnBtnYearSem);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(100, 10, 3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1180, 42);
+            this.panel1.TabIndex = 2;
+            // 
+            // iconButton1
+            // 
+            this.iconButton1.BackColor = System.Drawing.Color.Gray;
+            this.iconButton1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.iconButton1.FlatAppearance.BorderSize = 0;
+            this.iconButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.iconButton1.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.iconButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iconButton1.ForeColor = System.Drawing.Color.White;
+            this.iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.iconButton1.IconColor = System.Drawing.Color.Black;
+            this.iconButton1.IconSize = 16;
+            this.iconButton1.Location = new System.Drawing.Point(852, 0);
+            this.iconButton1.Name = "iconButton1";
+            this.iconButton1.Rotation = 0D;
+            this.iconButton1.Size = new System.Drawing.Size(328, 42);
+            this.iconButton1.TabIndex = 3;
+            this.iconButton1.Text = "Sub Group Management";
+            this.iconButton1.UseVisualStyleBackColor = false;
+            this.iconButton1.Click += new System.EventHandler(this.iconButton1_Click);
+            // 
+            // icnBtnGroup
+            // 
+            this.icnBtnGroup.BackColor = System.Drawing.Color.Gray;
+            this.icnBtnGroup.Dock = System.Windows.Forms.DockStyle.Left;
+            this.icnBtnGroup.FlatAppearance.BorderSize = 0;
+            this.icnBtnGroup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.icnBtnGroup.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.icnBtnGroup.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.icnBtnGroup.ForeColor = System.Drawing.Color.White;
+            this.icnBtnGroup.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.icnBtnGroup.IconColor = System.Drawing.Color.Black;
+            this.icnBtnGroup.IconSize = 16;
+            this.icnBtnGroup.Location = new System.Drawing.Point(586, 0);
+            this.icnBtnGroup.Name = "icnBtnGroup";
+            this.icnBtnGroup.Rotation = 0D;
+            this.icnBtnGroup.Size = new System.Drawing.Size(266, 42);
+            this.icnBtnGroup.TabIndex = 2;
+            this.icnBtnGroup.Text = "Group Management";
+            this.icnBtnGroup.UseVisualStyleBackColor = false;
+            this.icnBtnGroup.Click += new System.EventHandler(this.icnBtnGroup_Click);
+            // 
+            // icnBtnProgram
+            // 
+            this.icnBtnProgram.BackColor = System.Drawing.Color.Gray;
+            this.icnBtnProgram.Dock = System.Windows.Forms.DockStyle.Left;
+            this.icnBtnProgram.FlatAppearance.BorderSize = 0;
+            this.icnBtnProgram.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.icnBtnProgram.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.icnBtnProgram.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.icnBtnProgram.ForeColor = System.Drawing.Color.White;
+            this.icnBtnProgram.IconChar = FontAwesome.Sharp.IconChar.None;
+            this.icnBtnProgram.IconColor = System.Drawing.Color.Black;
+            this.icnBtnProgram.IconSize = 16;
+            this.icnBtnProgram.Location = new System.Drawing.Point(325, 0);
+            this.icnBtnProgram.Name = "icnBtnProgram";
+            this.icnBtnProgram.Rotation = 0D;
+            this.icnBtnProgram.Size = new System.Drawing.Size(261, 42);
+            this.icnBtnProgram.TabIndex = 1;
+            this.icnBtnProgram.Text = "Program Management";
+            this.icnBtnProgram.UseVisualStyleBackColor = false;
+            this.icnBtnProgram.Click += new System.EventHandler(this.icnBtnProgram_Click);
+            // 
+            // panelStudentChild
+            // 
+            this.panelStudentChild.BackColor = System.Drawing.Color.White;
+            this.panelStudentChild.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelStudentChild.Location = new System.Drawing.Point(0, 42);
+            this.panelStudentChild.Name = "panelStudentChild";
+            this.panelStudentChild.Size = new System.Drawing.Size(1180, 641);
+            this.panelStudentChild.TabIndex = 3;
+            // 
+            // StudentMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1180, 683);
+            this.Controls.Add(this.panelStudentChild);
+            this.Controls.Add(this.panel1);
+            this.Name = "StudentMain";
+            this.Text = "StudentMain";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FontAwesome.Sharp.IconButton icnBtnYearSem;
+        private System.Windows.Forms.Panel panel1;
+        private FontAwesome.Sharp.IconButton icnBtnGroup;
+        private FontAwesome.Sharp.IconButton icnBtnProgram;
+        private System.Windows.Forms.Panel panelStudentChild;
+        private FontAwesome.Sharp.IconButton iconButton1;
+    }
+}
---
+++ Student/SubGroup.Designer.cs
@@ -0,0 +1,234 @@
+﻿using CollegeManagement;
+
+namespace CollegeCore.Student
+{
+    partial class SubGroup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.collegeDBDataSet = new CollegeManagement.CollegeDBDataSet();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSave = new FontAwesome.Sharp.IconButton();
+            this.btnUpdate = new FontAwesome.Sharp.IconButton();
+            this.btnDelete = new FontAwesome.Sharp.IconButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupsTableAdapter = new CollegeManagement.CollegeDBDataSetTableAdapters.groupsTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.groupsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.collegeDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(409, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SubGroup Management";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(175, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Group ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(175, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(124, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sub Group ID";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DataSource = this.groupsBindingSource;
+            this.comboBox1.DisplayMember = "group_id";
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(484, 112);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(275, 30);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.ValueMember = "group_id";
+            // 
+            // groupsBindingSource
+            // 
+            this.groupsBindingSource.DataMember = "groups";
+            this.groupsBindingSource.DataSource = this.collegeDBDataSet;
+            // 
+            // collegeDBDataSet
+            // 
+            this.collegeDBDataSet.DataSetName = "CollegeDBDataSet";
+            this.collegeDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(484, 186);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(275, 28);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Gray;
+            this.btnSave.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSave.IconChar = FontAwesome.Sharp.IconChar.Save;
+            this.btnSave.IconColor = System.Drawing.Color.White;
+            this.btnSave.IconSize = 32;
+            this.btnSave.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSave.Location = new System.Drawing.Point(989, 391);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Rotation = 0D;
+            this.btnSave.Size = new System.Drawing.Size(146, 47);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "Save";
+            this.btnSave.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Gray;
+            this.btnUpdate.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnUpdate.IconChar = FontAwesome.Sharp.IconChar.Edit;
+            this.btnUpdate.IconColor = System.Drawing.Color.White;
+            this.btnUpdate.IconSize = 32;
+            this.btnUpdate.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnUpdate.Location = new System.Drawing.Point(989, 470);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Rotation = 0D;
+            this.btnUpdate.Size = new System.Drawing.Size(146, 54);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Gray;
+            this.btnDelete.Flip = FontAwesome.Sharp.FlipOrientation.Normal;
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnDelete.IconChar = FontAwesome.Sharp.IconChar.Trash;
+            this.btnDelete.IconColor = System.Drawing.Color.WhiteSmoke;
+            this.btnDelete.IconSize = 32;
+            this.btnDelete.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnDelete.Location = new System.Drawing.Point(989, 551);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Rotation = 0D;
+            this.btnDelete.Size = new System.Drawing.Size(146, 53);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightGray;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(77, 280);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(707, 292);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // groupsTableAdapter
+            // 
+            this.groupsTableAdapter.ClearBeforeFill = true;
+            // 
+            // SubGroup
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1180, 653);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "SubGroup";
+            this.Text = "SubGroup";
+            this.Load += new System.EventHandler(this.SubGroup_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.groupsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.collegeDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private FontAwesome.Sharp.IconButton btnSave;
+        private FontAwesome.Sharp.IconButton btnUpdate;
+        private FontAwesome.Sharp.IconButton btnDelete;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private CollegeDBDataSet collegeDBDataSet;
+        private System.Windows.Forms.BindingSource groupsBindingSource;
+        private CollegeManagement.CollegeDBDataSetTableAdapters.groupsTableAdapter groupsTableAdapter;
+        //private CollegeDBDataSetTableAdapters.groupTableAdapter groupTableAdapter;
+    }
+}
